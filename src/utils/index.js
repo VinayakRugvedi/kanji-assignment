a8b64ef536383fdb9fbfc7e1f6db8d5de6b35bb9
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-20 17:26:43 +0530

    create utils dir and add nftItems data

---
+++ src/utils/index.js
@@ -0,0 +1,74 @@
+const nftItems = [
+    {
+        name: "Cool Cats #001",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #002",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #003",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #004",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #005",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #006",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #007",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #008",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #009",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    },
+    {
+        name: "Cool Cats #010",
+        properties: {},
+        isComplete: false,
+        statusString: "Incomplete",
+        imageUrl: ""
+    }
+]
+
+export {nftItems}
