d3be55a0930dbfbe8eb9bf72a6b696107fe39993
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-21 18:31:59 +0530

    add id's and imageUrls

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,73 +1,88 @@
+// https://github.com/Laosing/cute-cat-avatars
+
 const nftItems = [
     {
+        id: 1,
         name: "Cool Cats #001",
-        properties: {},
+        properties: {
+            rarity: ["rare"],
+            eyes: ["blue", "green"]
+        },
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/announcer"
     },
     {
+        id: 2,
         name: "Cool Cats #002",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/idea"
     },
     {
+        id: 3,
         name: "Cool Cats #003",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/bug"
     },
     {
+        id: 4,
         name: "Cool Cats #004",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/art"
     },
     {
+        id: 5,
         name: "Cool Cats #005",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/award"
     },
     {
+        id: 6,
         name: "Cool Cats #006",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/gaming"
     },
     {
+        id: 7,
         name: "Cool Cats #007",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/news"
     },
     {
+        id: 8,
         name: "Cool Cats #008",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/tv"
     },
     {
+        id: 9,
         name: "Cool Cats #009",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/comic"
     },
     {
+        id: 10,
         name: "Cool Cats #010",
         properties: {},
         isComplete: false,
         statusString: "Incomplete",
-        imageUrl: ""
+        imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/book"
     }
 ]
 
