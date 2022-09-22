42d91f4ddd30c47ffed28709cc7e4bcbdfe81afc
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-22 19:16:08 +0530

    add state and handler, consume them

--- src/pages/CreateNft/CreateNft.container.js
+++ src/pages/CreateNft/CreateNft.container.js
@@ -1,7 +1,46 @@
+import {useState} from "react";
+
+import {nftItems} from "utils";
 import CreateNft from "./CreateNft";
 
+const getNftItemsMapping = () => {
+    const itemsMapping = {};
+    for (let item of nftItems) {
+        itemsMapping[item.id] = item
+    }
+    return itemsMapping;
+};
+
 const CreateNftContainer = () => {
-    return (<CreateNft />);
+    const [itemsMapping, setItemsMapping] = useState(getNftItemsMapping());
+    const [selectedItemsMapping, setSelectedItemsMapping] = useState({});
+
+    const handleItemSelection = (selectedItem = null) => {
+        if (!selectedItem) {
+            if (Object.keys(selectedItemsMapping).length === Object.keys(itemsMapping).length) {
+                setSelectedItemsMapping({});
+            } else {
+                const itemsMappingCopy = {...itemsMapping};
+                setSelectedItemsMapping(itemsMappingCopy);
+            }
+        } else {
+            const selectedItemsMappingCopy = {...selectedItemsMapping};
+            if (selectedItem.id in selectedItemsMapping) {
+                delete selectedItemsMappingCopy[selectedItem.id]
+            } else {
+                selectedItemsMappingCopy[selectedItem.id] = selectedItem;
+            }
+            setSelectedItemsMapping(selectedItemsMappingCopy);
+        }
+    };
+
+    return (
+        <CreateNft
+            itemsMapping={itemsMapping}
+            selectedItemsMapping={selectedItemsMapping}
+            handleItemSelection={handleItemSelection}
+        />
+    );
 }
 
 export default CreateNftContainer;
--- src/pages/CreateNft/CreateNft.js
+++ src/pages/CreateNft/CreateNft.js
@@ -1,15 +1,48 @@
+import PropTypes from "prop-types";
+
+import {Button} from "components";
+
+import {ButtonTypes} from "types";
+import {Table} from "./components";
+
 import "./CreateNft.css";
 
-const CreateNft = () => {
+const propTypes = {
+    itemsMapping: PropTypes.object.isRequired,
+    selectedItemsMapping: PropTypes.object.isRequired,
+    handleItemSelection: PropTypes.func.isRequired
+};
+
+const CreateNft = ({
+    itemsMapping,
+    selectedItemsMapping,
+    handleItemSelection
+}) => {
+    const isItemsSelected = Object.keys(selectedItemsMapping).length > 0;
+
     return (
         <div className="create-nft-page-container">
             <div className="sub-header-container">
-                <h5>Cool Cats</h5>
+                <div className="title-container">
+                    <h2 className="title">Cool Cats</h2>
+                    <span className="description">10 NFTs added</span>
+                </div>
                 <p>Search</p>
-                <button>Edit properties</button>
+                <div className={`visibility-${isItemsSelected ? "visible" : "hidden"}`}>
+                    <Button type={ButtonTypes.Outline}>Edit Properties</Button>
+                </div>
+            </div>
+            <div>
+                <Table
+                    itemsMapping={itemsMapping}
+                    handleItemSelection={handleItemSelection}
+                    selectedItemsMapping={selectedItemsMapping}
+                />
             </div>
         </div>
     );
 }
+
+CreateNft.propTypes = propTypes;
 
 export default CreateNft;
