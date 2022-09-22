8858ed1cc4b6d41c3e974aa6ba90a1a22aa1d362
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-22 19:06:53 +0530

    create Input components dir and add Checkbox component uder it

---
+++ src/components/Input/Checkbox/Checkbox.js
@@ -0,0 +1,50 @@
+import {useState} from "react";
+import PropTypes from "prop-types";
+import "./Checkbox.css";
+
+const propTypes = {
+    checked: PropTypes.bool,
+    label: PropTypes.string,
+    handleOnChange: PropTypes.func
+};
+
+const defaultProps = {
+    checked: false,
+    label: "",
+    handleOnChange: () => {}
+}
+
+const Checkbox = ({
+    checked,
+    label,
+    handleOnChange,
+    ...props
+}) => {
+    const defaultChecked = checked ? checked : false;
+    const [isChecked, setIsChecked] = useState(defaultChecked);
+
+    const onChange = () => {
+        setIsChecked((previous) => !previous);
+        handleOnChange();
+    };
+
+    return (
+      <div className="custom-checkbox-container">
+        <label>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={onChange}
+            className={isChecked ? "checked" : ""}
+            {...props}
+          />
+          <span>{label}</span>
+        </label>
+      </div>
+    )
+};
+
+Checkbox.propTypes = propTypes;
+Checkbox.defaultProps = defaultProps;
+
+export default Checkbox;
