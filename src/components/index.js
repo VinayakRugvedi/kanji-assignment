854e1170ce13bb8151e101a98b42b256cd819a9e
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-22 19:15:36 +0530

    export InputCheckbox

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,11 @@ import Label from "./Label/Label";
 import Button from "./Button/Button";
 import Avatar from "./Avatar/Avatar";
 
+import {InputCheckbox} from "./Input"
+
 export {
     Label,
     Button,
-    Avatar
+    Avatar,
+    InputCheckbox
 };
