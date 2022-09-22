1d749bdde692f0829880f0ed93d2a10eeccb79ee
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-22 19:07:57 +0530

    make ids as strings

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@
 
 const nftItems = [
     {
-        id: 1,
+        id: "1",
         name: "Cool Cats #001",
         properties: {
             rarity: ["rare"],
@@ -13,7 +13,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/announcer"
     },
     {
-        id: 2,
+        id: "2",
         name: "Cool Cats #002",
         properties: {},
         isComplete: false,
@@ -21,7 +21,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/idea"
     },
     {
-        id: 3,
+        id: "3",
         name: "Cool Cats #003",
         properties: {},
         isComplete: false,
@@ -29,7 +29,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/bug"
     },
     {
-        id: 4,
+        id: "4",
         name: "Cool Cats #004",
         properties: {},
         isComplete: false,
@@ -37,7 +37,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/art"
     },
     {
-        id: 5,
+        id: "5",
         name: "Cool Cats #005",
         properties: {},
         isComplete: false,
@@ -45,7 +45,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/award"
     },
     {
-        id: 6,
+        id: "6",
         name: "Cool Cats #006",
         properties: {},
         isComplete: false,
@@ -61,7 +61,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/news"
     },
     {
-        id: 8,
+        id: "8",
         name: "Cool Cats #008",
         properties: {},
         isComplete: false,
@@ -69,7 +69,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/tv"
     },
     {
-        id: 9,
+        id: "9",
         name: "Cool Cats #009",
         properties: {},
         isComplete: false,
@@ -77,7 +77,7 @@ const nftItems = [
         imageUrl: "https://cute-cat-avatars.fly.dev/api/v1/comic"
     },
     {
-        id: 10,
+        id: "10",
         name: "Cool Cats #010",
         properties: {},
         isComplete: false,
