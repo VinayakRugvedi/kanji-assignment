1c3d567d317a5e10c0a23ec9bc3f07087f05fe88
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-23 14:07:39 +0530

    add handleSearch and update getNftItems for performance

--- src/pages/CreateNft/CreateNft.container.js
+++ src/pages/CreateNft/CreateNft.container.js
@@ -6,8 +6,25 @@ import CreateNft from "./CreateNft";
 const getNftItemsMapping = () => {
     const itemsMapping = {};
     for (let item of nftItems) {
-        itemsMapping[item.id] = item
+        itemsMapping[item.id] = item;
+
+        const propertyNames = Object.keys(item.properties);
+        const transformedNames = [];
+        for (let name of propertyNames) {
+            transformedNames.push(name[0].toUpperCase() + name.slice(1, name.length))
+        }
+        itemsMapping[item.id].propertyNamesString = transformedNames.length > 0 ? transformedNames.join(" | ") : "";
+
+        const values = Object.values(item.properties);
+        const transformedValues = [];
+        for (let value of values) {
+            for (let valueItem of value) {
+                transformedValues.push(valueItem[0].toUpperCase() + valueItem.slice(1, valueItem.length))
+            }
+        }
+        itemsMapping[item.id].propertyValuesString = transformedValues.length > 0 ? transformedValues.join(" | ") : "";
     }
+
     return itemsMapping;
 };
 
@@ -34,12 +51,32 @@ const CreateNftContainer = () => {
         }
     };
 
+    const handleSearch = (searchString = "") => {
+        searchString = searchString.toLowerCase();
+        const nftItemsMapping = getNftItemsMapping();
+        if (searchString.length === 0) {
+            setItemsMapping(nftItemsMapping);
+        } else {
+            const searchResults = {};
+            for (let item in nftItemsMapping) {
+                const searchContextString = `${nftItemsMapping[item].propertyNamesString.toLowerCase()} ${nftItemsMapping[item].propertyValuesString.toLowerCase()}`;
+                if (searchContextString.includes(searchString)) {
+                    searchResults[item] = nftItemsMapping[item];
+                }
+            }
+            setItemsMapping(searchResults);
+        }
+    };
+
     return (
-        <CreateNft
-            itemsMapping={itemsMapping}
-            selectedItemsMapping={selectedItemsMapping}
-            handleItemSelection={handleItemSelection}
-        />
+        <>
+            <CreateNft
+                itemsMapping={itemsMapping}
+                selectedItemsMapping={selectedItemsMapping}
+                handleItemSelection={handleItemSelection}
+                handleSearch={handleSearch}
+            />
+        </>
     );
 }
 
