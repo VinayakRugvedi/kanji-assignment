c6b69fdaab0152f929c948cb19c5bd062a99a7cf
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-09-25 19:26:21 +0530

    export Select

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,11 @@
 import InputCheckbox from "./Checkbox/Checkbox";
 import InputSearch from "./Search/Search";
 import InputText from "./Text/Text";
+import InputSelect from "./Select/Select";
 
 export {
     InputCheckbox,
     InputSearch,
-    InputText
+    InputText,
+    InputSelect
 };
